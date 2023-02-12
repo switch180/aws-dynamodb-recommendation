e93b518065199d6facf2b10ce1be1bba627b9985
Fedayizada <fedayizada@pm.me>
2023-02-11 23:21:27 -0500

    fix Athena Query

--- src/athenaquery.py
+++ src/athenaquery.py
@@ -37,8 +37,8 @@ def create_cost_estimate(params):
         , date_trunc('hour', CAST(timestamp AS timestamp)) "timestamp"
         , sum(unit) "Consumed_unit"
         , avg(estUnit) "Provisioned_Estunit"
-        , (CASE WHEN (metric_name = 'ConsumedWriteCapacityUnits') THEN '%s' WHEN (metric_name = 'ConsumedReadCapacityUnits') THEN '%s' ELSE null END) min_capacity
-        , (CASE WHEN (metric_name = 'ConsumedWriteCapacityUnits') THEN '%s' WHEN (metric_name = 'ConsumedReadCapacityUnits') THEN '%s' ELSE null END) target_utilization
+        , (CASE WHEN (metric_name = 'ConsumedWriteCapacityUnits') THEN %s WHEN (metric_name = 'ConsumedReadCapacityUnits') THEN %s ELSE null END) min_capacity
+        , (CASE WHEN (metric_name = 'ConsumedWriteCapacityUnits') THEN %s WHEN (metric_name = 'ConsumedReadCapacityUnits') THEN %s ELSE null END) target_utilization
         , (CASE WHEN (metric_name = 'ConsumedWriteCapacityUnits') THEN 'ProvisionedWriteCapacityUnits' WHEN (metric_name = 'ConsumedReadCapacityUnits') THEN 'ProvisionedReadCapacityUnits' ELSE null END) metric_name
         , (CASE WHEN (metric_name = 'ConsumedWriteCapacityUnits') THEN ((sum(unit) / 1000000) * 1.25E0) WHEN (metric_name = 'ConsumedReadCapacityUnits') THEN ((sum(unit) / 1000000) * 2.5E-1) ELSE null END) ondemand_cost
         , (CASE WHEN (metric_name = 'ConsumedReadCapacityUnits') THEN (avg(estUnit) * 1.3E-4) WHEN (metric_name = 'ConsumedWriteCapacityUnits') THEN (avg(estUnit) * 6.5E-4) ELSE null END) est_provisoned_cost
@@ -82,34 +82,55 @@ def create_dynamo_mode_recommendation(params):
   
 
     intialqu = """CREATE OR REPLACE VIEW "%s_recommendation" AS 
+                SELECT *
+            , (CASE WHEN (current_mode <> recommended_mode) THEN 'Not Optimized' ELSE 'Optimized' END) status
+            , CASE
+            WHEN current_mode = 'Ondemand' AND recommended_mode = 'Provisioned'
+            THEN (ondemand_cost - est_provisioned_cost) / ondemand_cost * 100
+            WHEN current_mode = 'Provisioned' AND recommended_mode = 'Ondemand'
+            THEN (current_provisioned_cost - ondemand_cost) / current_provisioned_cost * 100
+            WHEN current_mode = 'Provisioned' AND recommended_mode = 'Provisioned_Modify'
+            THEN (current_provisioned_cost - est_provisioned_cost) / current_provisioned_cost * 100
+            ELSE NULL
+        END AS savings_pct
+        FROM (
+
         SELECT
         q1.index_name
         , q1.base_table_name
         , q1.accountid
+        , q1.metric_name
         , q1.est_provisioned_cost
         , q1.current_provisioned_cost
         , q1.Ondemand_cost
         , q1.recommended_mode
-        , (CASE WHEN (%s_dynamodb_info.base_table_name IS NOT NULL) THEN 'Provisioned' ELSE 'Ondemand' END) current_mode
+        , (CASE WHEN (q2.base_table_name IS NOT NULL) THEN 'Provisioned' ELSE 'Ondemand' END) current_mode
         , q1.number_of_days
+        , q2.min_capacity current_min_capacity
+        , q1.min_capacity simulated_min_capacity
+        , q2.target_utilization current_target_utilization
+        , q1.target_utilization simulated_target_utilization
         FROM
         ((
         SELECT
             "name" "index_name"
         , split_part("name", ':', 1) "base_table_name"
         , "accountid"
+        , "metric_name"
         , "region"
         , "sum"("est_provisioned_cost") "est_provisioned_cost"
         , "sum"("current_provisioned_cost") current_provisioned_cost
         , "sum"("ondemand_cost") "Ondemand_cost"
         , (CASE WHEN (("sum"("est_provisioned_cost") < "sum"("current_provisioned_cost")) AND ((("sum"("current_provisioned_cost") - "sum"("est_provisioned_cost")) / "sum"("current_provisioned_cost")) > 1.5E-1) AND ("sum"("est_provisioned_cost") < "sum"("ondemand_cost"))) THEN 'Provisioned_Modify' WHEN (("sum"("current_provisioned_cost") > "sum"("ondemand_cost")) OR ("sum"("est_provisioned_cost") > "sum"("ondemand_cost"))) THEN 'Ondemand' ELSE 'Provisioned' END) "recommended_mode"
-        , EXTRACT(DAY FROM (MAX(timestamp) - MIN(timestamp))) "number_of_days"
+        , (EXTRACT(DAY FROM (MAX(timestamp) - MIN(timestamp))) + 1) "number_of_days"
+        , avg(min_capacity) min_capacity
+        , avg(target_utilization) target_utilization
         FROM
             %s_cost_estimate
-        GROUP BY "name", split_part("name", ':', 1), "accountid", "region"
+        GROUP BY "name", split_part("name", ':', 1), "accountid", "region" , "metric_name"
         )  q1
-        LEFT JOIN %s_dynamodb_info ON ((%s_dynamodb_info.base_table_name = q1.base_table_name) AND (%s_dynamodb_info.index_name = q1.index_name) AND (%s_dynamodb_info.region = q1.region)))"""
-    costmodequ = intialqu % (tablename, tablename,tablename,tablename,tablename,tablename,tablename)
+        LEFT JOIN %s_dynamodb_info q2 ON ((q2.base_table_name = q1.base_table_name) AND (q2.index_name = q1.index_name) AND (q2.metric_name = q1.metric_name) AND (q2.region = q1.region))))"""
+    costmodequ = intialqu % (tablename, tablename,tablename)
     params = {
         'database': database,
         'bucket': bucket,
@@ -130,7 +151,7 @@ def create_dynamo_autoscaling_recommendation(params):
     path = params['athena_bucket_prefix']
 
     intialqu = """CREATE OR REPLACE VIEW "%s_autoscaling_sizing" AS 
-        SELECT *
+        SELECT * , (((current_provisioned_cost - est_provisioned_cost) / current_provisioned_cost) * 100) savings_percentage
         FROM
         (
         SELECT
@@ -143,6 +164,8 @@ def create_dynamo_autoscaling_recommendation(params):
         , min(target_utilization) current_min_target_utilization
         , supplied_min_capacity
         , supplied_target_utilization
+        , sum(current_provisioned_cost) current_provisioned_cost
+        , sum(est_provisioned_cost) est_provisioned_cost
         FROM
             (
             SELECT
@@ -156,12 +179,16 @@ def create_dynamo_autoscaling_recommendation(params):
             , "p"."EstUnit"
             , "p"."supplied_min_capacity"
             , "p"."supplied_target_utilization"
+            , "P"."est_provisioned_cost"
+            , "P"."current_provisioned_cost"
             FROM
                 ((
                 SELECT
                 "name"
                 , "region"
                 , "accountid"
+                , sum(est_provisioned_cost) est_provisioned_cost
+                 , sum(current_provisioned_cost) current_provisioned_cost
                 , "date_trunc"('hour', CAST("timestamp" AS timestamp)) "timestamp"
                 , "metric_name"
                 , "avg"("est_provisioned_unit") "estUnit"
